452ae36169b475ae705b08834ac713215512430c
caio-ferreira-dev <caioferreirasdj@gmail.com>
2024-03-15 18:33:05 -0300

    first commit

---
+++ src/components/BatButton/BatButton.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Text, Pressable } from 'react-native';
+import * as Clipboard from 'expo-clipboard';
+
+import { styles } from './BatButtonStyles';
+import { BatTextInput } from '../BatTextInput/BatTextInput';
+import generatePass from '../../services/passwordService';
+
+export function BatButton() {
+  const [pass, setPass] = useState('');
+    
+  function handleGenerateButton() {
+    let generateToken = generatePass()
+    setPass(generateToken)
+  }
+
+  function handleCopyButton() {
+    Clipboard.setStringAsync(pass)
+  }
+  return (
+    <>
+        <BatTextInput pass={pass}/>
+        <Pressable onPress={handleGenerateButton} style={styles.button}>
+            <Text style={styles.text}>Generate</Text>
+        </Pressable>
+        <Pressable onPress={handleCopyButton} style={styles.button}>
+            <Text style={styles.text}>⚡ Copy</Text>
+        </Pressable>
+    </>
+  );
+}
---
+++ src/components/BatButton/BatButtonStyles.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    width: '100%',
+    justifyContent: 'center',
+    marginTop: 5,
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    backgroundColor: 'black',
+    borderRadius: 4,
+    elevation: 3,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: '#E5BF3C'
+
+  }
+});
